a3dfd42e64f412e72da7362aedad20590702d348
brianeflin <brian.eflin@gmail.com>
2017-03-25 21:40:45 -0500

    Ch01 commit

---
+++ Ch01/Exercises/Exercises/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current date/time: " + DateTime.Now);
+            Console.WriteLine("The square root of 12345: " + Math.Sqrt(12345));
+
+            for (int i = 2; i < 103; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(-i);
+                }
+            }
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("In 10 years you will be " + (age + 10) + " years old.");
+
+            Console.ReadKey();
+        }
+    }
+}
